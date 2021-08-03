8f7264f8a94f550dbf60e0e13407cca04c0a14c5
Pedro Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2021-08-03 20:13:30 -0300

    Update validates_correios_cep_spec.rb

--- spec/validates_correios_cep_spec.rb
+++ spec/validates_correios_cep_spec.rb
@@ -1,11 +1,88 @@
 # frozen_string_literal: true
 
 RSpec.describe ValidatesCorreiosCep do
-  it 'successfully example test' do
-    MyAddress.first
-    MyAddress2.first
-    MyPoroAddress.new
-    MyPoroAddress2.new
-    expect(false).to eq false
+  before :each do
+    @my_address = MyAddress.new
+    @my_address2 = MyAddress2.new
+    @my_poro_address = MyPoroAddress.new
+    @my_poro_address2 = MyPoroAddress2.new
+    @klasses = [@my_address, @my_address2, @my_poro_address, @my_poro_address2]
+  end
+
+  describe 'ActiveRecord' do
+    describe 'built-in validations' do
+      it 'valid' do
+        @klasses.each do |klass_instance|
+          klass_instance.zipcode = '01131010'
+          expect(klass_instance.valid?).to eq true
+          expect(klass_instance.errors.size).to eq 0
+        end
+      end
+
+      it 'not_exists' do
+      end
+
+      it 'connection_failed' do
+      end
+
+      it 'invalid' do
+      end
+
+      it 'timeouted' do
+      end
+    end
+
+    describe 'validation custom method' do
+      it 'valid' do
+      end
+
+      it 'not_exists' do
+      end
+
+      it 'connection_failed' do
+      end
+
+      it 'invalid' do
+      end
+
+      it 'timeouted' do
+      end
+    end
+  end
+
+  describe 'ActiveModel' do
+    describe 'built-in validations' do
+      it 'valid' do
+      end
+
+      it 'not_exists' do
+      end
+
+      it 'connection_failed' do
+      end
+
+      it 'invalid' do
+      end
+
+      it 'timeouted' do
+      end
+    end
+
+    describe 'validation custom method' do
+      it 'valid' do
+      end
+
+      it 'not_exists' do
+      end
+
+      it 'connection_failed' do
+      end
+
+      it 'invalid' do
+      end
+
+      it 'timeouted' do
+      end
+    end
   end
 end
