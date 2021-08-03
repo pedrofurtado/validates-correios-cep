326dc7d4729578e3379ea8374f32b4e60f84f73d
Pedro Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2021-08-03 20:46:42 -0300

    Update activemodel.rb

--- lib/validates-correios-cep/activemodel.rb
+++ lib/validates-correios-cep/activemodel.rb
@@ -8,12 +8,12 @@ module ActiveModel
 
       def validate_each(record, attribute, value)
         error_message_scope = begin
-                                "#{CORREIOS_CEP_I18N_SCOPE}.not_exists" if Correios::CEP::AddressFinder.get(value).blank?
+                                "#{CORREIOS_CEP_I18N_SCOPE}.not_exists" if ::Correios::CEP::AddressFinder.get(value).blank?
                               rescue EOFError
                                 "#{CORREIOS_CEP_I18N_SCOPE}.connection_failed"
                               rescue ArgumentError
                                 "#{CORREIOS_CEP_I18N_SCOPE}.invalid"
-                              rescue Net::OpenTimeout
+                              rescue ::Net::OpenTimeout
                                 "#{CORREIOS_CEP_I18N_SCOPE}.timeouted"
                               end
 
