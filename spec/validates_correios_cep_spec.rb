8075c2400bef9bb1b208086d05f81343fb6a852f
Pedro Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2021-08-03 20:25:01 -0300

    Update validates_correios_cep_spec.rb

--- spec/validates_correios_cep_spec.rb
+++ spec/validates_correios_cep_spec.rb
@@ -2,6 +2,7 @@
 
 RSpec.describe ValidatesCorreiosCep do
   before :each do
+    @i18n_scope = ::ActiveModel::Validations::CorreiosCepValidator::CORREIOS_CEP_I18N_SCOPE
     @my_address = MyAddress.new
     @my_address2 = MyAddress2.new
     @my_poro_address = MyPoroAddress.new
@@ -9,80 +10,29 @@ RSpec.describe ValidatesCorreiosCep do
     @klasses = [@my_address, @my_address2, @my_poro_address, @my_poro_address2]
   end
 
-  describe 'ActiveRecord' do
-    describe 'built-in validations' do
-      it 'valid' do
-        @klasses.each do |klass_instance|
-          klass_instance.zipcode = '011310106'
-          expect(klass_instance.valid?).to eq true
-          expect(klass_instance.errors.size).to eq 0
-        end
-      end
-
-      it 'not_exists' do
-      end
-
-      it 'connection_failed' do
-      end
-
-      it 'invalid' do
-      end
-
-      it 'timeouted' do
-      end
-    end
-
-    describe 'validation custom method' do
-      it 'valid' do
-      end
-
-      it 'not_exists' do
-      end
-
-      it 'connection_failed' do
-      end
-
-      it 'invalid' do
-      end
-
-      it 'timeouted' do
-      end
+  it 'valid' do
+    @klasses.each do |klass_instance|
+      klass_instance.zipcode = '01131010'
+      expect(klass_instance.valid?).to eq true
+      expect(klass_instance.errors.size).to eq 0
     end
   end
 
-  describe 'ActiveModel' do
-    describe 'built-in validations' do
-      it 'valid' do
-      end
-
-      it 'not_exists' do
-      end
-
-      it 'connection_failed' do
-      end
-
-      it 'invalid' do
-      end
-
-      it 'timeouted' do
-      end
+  it 'not_exists' do
+    @klasses.each do |klass_instance|
+      klass_instance.zipcode = '01234567'
+      expect(klass_instance.valid?).to eq false
+      expect(klass_instance.errors.size).to eq 1
+      expect(klass_instance.errors.full_messages.include?(I18n.t("#{@i18n_scope}.invalid", zipcode: '01234567'))).to eq true
     end
+  end
 
-    describe 'validation custom method' do
-      it 'valid' do
-      end
+  it 'connection_failed' do
+  end
 
-      it 'not_exists' do
-      end
+  it 'invalid' do
+  end
 
-      it 'connection_failed' do
-      end
-
-      it 'invalid' do
-      end
-
-      it 'timeouted' do
-      end
-    end
+  it 'timeouted' do
   end
 end
